b62064ad9acbce14b5338fc6e4395b7c88bd1b48
wishMeLuck9 <149300259+wishMeLuck9@users.noreply.github.com>
2026-04-01 07:47:37 +0100

    1

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
