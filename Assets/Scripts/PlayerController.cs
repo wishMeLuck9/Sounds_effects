2946e5a7353f33137caa9cb920d74d43e0759ee3
wishMeLuck9 <149300259+wishMeLuck9@users.noreply.github.com>
2026-04-01 07:12:10 +0100

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 1000);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
